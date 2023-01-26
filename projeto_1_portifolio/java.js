c872f852c264e1c84b024394c9cc8564641455e2
unknown <gulopespai025@gmail.com>
2023-01-26 00:39:59 -0300

    primeiro commit

---
+++ projeto_1_portifolio/java.js
@@ -0,0 +1,9 @@
+function abrirmenu(){
+    let barramenuaberto = document.getElementById('barra-menu')
+    if(barramenuaberto.style.width == "0px"){
+        barramenuaberto.style.width == "180px"
+
+    }else{
+        barramenuaberto.style.width == "0px"
+    }
+}
